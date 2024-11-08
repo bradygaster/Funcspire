e0792579152ded7fdc9ffd461fdd89390f621783
bradygaster <bradyg@microsoft.com>
2024-11-08 14:12:26 -0800

    changed project name

--- Funcspire.AppHost/Program.cs
+++ Funcspire.AppHost/Program.cs
@@ -1,6 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var functions = builder.AddAzureFunctionsProject<Projects.Funcspire>("functions");
+var functions = builder.AddAzureFunctionsProject<Projects.Funcspire_Functions>("functions");
 
 var frontend = builder.AddProject<Projects.Frontend>("frontend")
                       .WithReference(functions);
